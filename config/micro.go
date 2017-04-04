e1bb3a3ef4f5df68244ee333b488f052db313d85
Brian Hechinger <wonko@4amlunch.net>
2017-04-04 12:48:15 -0400

    Fixed the micro test for all paths (including errors)
    also re-worked the way the micro method returns errors. Went with the idiomatic Go method.

--- config/micro.go
+++ config/micro.go
@@ -1,14 +1,17 @@
 package config
 
-import "github.com/micro/go-micro"
+import (
+	"github.com/micro/go-micro"
+	"errors"
+)
 
-func (c Configuration) NewService(version string) micro.Service {
+func (c Configuration) NewService(version string) (micro.Service, error) {
 	if !c.initialized {
-		panic("Configuration not initialized, call Load() before calling this.")
+		return nil, errors.New("Configuration not initialized, call Load() before calling this.")
 	}
 
 	return micro.NewService(
 		micro.Name(c.Namespace + "." + c.Name),
 		micro.Version(version),
-	)
+	), nil
 }
--- config/micro_test.go
+++ config/micro_test.go
@@ -8,7 +8,22 @@ import (
 func TestConfiguration_NewService(t *testing.T) {
 	conf := Configuration{}
 	conf.Load("application.dist.yaml")
-	if _, ok := conf.NewService("1.1.1").(micro.Service); !ok {
-		t.Error()
+	service, err := conf.NewService("1.1.1")
+	if _, ok := service.(micro.Service); !ok {
+		t.Errorf("service is not a micro.Service!")
+	}
+	if err != nil {
+		t.Errorf("Error not set to nil, message is: %s", err)
+	}
+}
+
+func TestConfiguration_NewService_NoConfLoaded(t *testing.T) {
+	conf := Configuration{}
+	service, err := conf.NewService("1.1.1")
+	if err == nil {
+		if _, ok := service.(micro.Service); ok {
+			t.Errorf("Error is nil and yet we have a valid service?")
+		}
+		t.Errorf("Error set to nil, at least we don't have a valid service")
 	}
 }
