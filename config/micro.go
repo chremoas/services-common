0b4759d923126674c6a566112e395dfdaf867db8
Matt Maurer <maurer.it@gmail.com>
2017-04-10 23:00:10 -0400

    MOAR GODOCS!

--- config/micro.go
+++ config/micro.go
@@ -8,6 +8,10 @@ import (
 
 var configurationFile string
 
+// Builds and inits a new micro.Service object for use.  The initFunc functor being asked for will be inserted
+// into the services options as a BeforeStart which will be called DURING the service.Run invocation but BEFORE the
+// service is fully up and operational.  All of your initialization code that you need should go into this initFunc.
+// If you don't need init code then feel free to use the NilInit function exported out of this package.
 func NewService(version string, initFunc func(configuration *Configuration) error) micro.Service {
 	service := micro.NewService(
 		micro.Version(version),
