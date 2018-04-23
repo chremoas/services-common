97fb09cce8f71fdc67cf02d6c55ff7a0a60d321b
Brian Hechinger <wonko@4amlunch.net>
2018-04-23 14:33:17 -0400

    All the role/sig/filter stuff is moved to a separate library.
    This was to de-duplicate the role/sig code as well as to prepare for the future plans of have an API service so this code could be re-used.

--- command/perform.go
+++ command/perform.go
@@ -1,22 +1,23 @@
 package command
 
 import (
+	"context"
 	permsrv "github.com/chremoas/perms-srv/proto"
 	"strings"
-	"context"
 )
 
 // Should perform.go setup its own client?
 type Permissions struct {
-	Client permsrv.PermissionsClient
+	Client          permsrv.PermissionsClient
+	PermissionsList []string
 }
 
-func (p Permissions) CanPerform(ctx context.Context, sender string, perms []string) (bool, error) {
+func (p Permissions) CanPerform(ctx context.Context, sender string) (bool, error) {
 	s := strings.Split(sender, ":")
 	canPerform, err := p.Client.Perform(ctx,
 		&permsrv.PermissionsRequest{
 			User:            s[1],
-			PermissionsList: perms,
+			PermissionsList: p.PermissionsList,
 		})
 
 	if err != nil {
